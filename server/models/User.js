a24b75f6e3c471b24c6697b73b07677a6ad4ba12
reibunny <alysonrezaire@outlook.fr>
2025-06-18 12:01:06 +0200

    feat: hash user password before saving

--- server/models/User.js
+++ server/models/User.js
@@ -1,4 +1,5 @@
 import { Schema, model } from "mongoose";
+import bcrypt from "bcrypt";
 
 const userSchema = new Schema({
     username: { type: String, required: true, unique: true },
@@ -9,5 +10,15 @@ const userSchema = new Schema({
     totalxp: { type: Number, default: 0 },
     avatar: String,
 });
+
+userSchema.pre("save", async function (next) {
+    if (!this.isModified("password")) return next();
+    this.password = await bcrypt.hash(this.password, 10);
+    next();
+});
+
+userSchema.methods.comparePassword = function (candidatePassword) {
+    return bcrypt.compare(candidatePassword, this.password);
+};
 
 export default model("User", userSchema);
