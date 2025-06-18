6dab1750d47615c50e2a9d00d796480211d557c2
reibunny <alysonrezaire@outlook.fr>
2025-06-18 11:58:30 +0200

    refactor(user): update User model structure

--- server/models/User.js
+++ server/models/User.js
@@ -1,9 +1,9 @@
 import { Schema, model } from "mongoose";
 
 const userSchema = new Schema({
-    username: String,
-    email: String,
-    password: String,
+    username: { type: String, required: true, unique: true },
+    email: { type: String, required: true, unique: true },
+    password: { type: String, required: true },
     level: { type: Number, default: 0 },
     xp: { type: Number, default: 0 },
     totalxp: { type: Number, default: 0 },
