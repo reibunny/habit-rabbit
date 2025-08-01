884028ad3fb225eb10b04a96dd64a141f87f8c1d
reibunny <alysonrezaire@outlook.fr>
2025-08-01 13:00:56 +0200

    feat: add router

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,10 +1,16 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import "./colors.css";
+import { createRoot } from "react-dom/client";
+import { BrowserRouter, Route, Routes } from "react-router";
+import Dashboard from "./pages/Dashboard.jsx";
+import App from "./App.jsx";
+import SignUp from "./pages/SignUp.jsx";
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+createRoot(document.getElementById("root")).render(
+    <BrowserRouter>
+        <Routes>
+            <Route path="/" element={<App />} />
+            <Route path="/register" element={<SignUp />} />
+            <Route path="/dashboard" element={<Dashboard />} />
+        </Routes>
+    </BrowserRouter>
+);
